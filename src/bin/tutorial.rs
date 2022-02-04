04a23233698f86afcceec0be56a702f33eda9bc1
0x04 <0x04@example.com>
2022-02-04 11:26:19 +0900

    feat: disguised admin

--- src/bin/tutorial.rs
+++ src/bin/tutorial.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use padding_oracle_attack::*;
+
+#[tokio::main]
+async fn main() {
+    let token = fetch_token().await;
+    println!("\n=== bytes ===");
+    println!("{:02x?}", &token);
+
+    let admin_token: Vec<u8> = disguised_admin(token.clone());
+
+    let msg = check_token(&admin_token).await;
+    println!("\n=== response ===");
+    println!("{}", &msg);
+}
+
+fn disguised_admin(mut token: Vec<u8>) -> Vec<u8> {
+    token[6] ^= b'9';
+    token[6] ^= b'0';
+
+    token
 }
