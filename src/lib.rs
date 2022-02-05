033693a19e51c90614fa6929ccc484ad9e2968cf
0x04 <0x04@example.com>
2022-02-05 20:37:33 +0900

    feat: decrypt token

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,26 @@ pub async fn fetch_token() -> Vec<u8> {
     base64::decode_config(token, base64::URL_SAFE).unwrap()
 }
 
+pub mod debug {
+    use super::BLOCK_SIZE;
+
+    // ref: https://github.com/kurenaif/padding_oracle_training/blob/1fa4ea4535a011c60080e192ca8769a4473bacf0/problem/app/app.rb#L7
+    const KEY: [u8; 32] = [b'a'; 32];
+
+    pub fn decrypt_token(token: &[u8]) -> Vec<u8> {
+        let (iv, cipher_text) = token.split_at(BLOCK_SIZE);
+        let cipher      = openssl::symm::Cipher::aes_256_cbc();
+        let mut decrypt = openssl::symm::Crypter::new(cipher, openssl::symm::Mode::Decrypt, &KEY, Some(iv)).unwrap();
+        let mut buffer  = vec![0u8; cipher_text.len() + cipher.block_size()];
+
+        decrypt.pad(false); // disable padding
+        decrypt.update(cipher_text, &mut buffer).unwrap();
+        decrypt.finalize(&mut buffer).unwrap();
+
+        let truncate_len = buffer.len() - cipher.block_size();
+        buffer.truncate(truncate_len);
+
+        buffer
+    }
+}
+
